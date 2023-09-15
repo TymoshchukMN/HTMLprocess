de462ce32769ecd3c31b670b261a007a8eef9427
TymoshchukMN <TymoshchukMN@gmail.com>
2023-09-15 22:22:00 +0300

    added HtmlAgilityPack

--- HTMLprocess/Program.cs
+++ HTMLprocess/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+using HtmlAgilityPack;
 
 namespace HTMLprocess
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var html = File.ReadAllText("E:\\data.txt", Encoding.UTF8);
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            List <string> names = new List<string>();
+
+            foreach (HtmlNode vol in doc.DocumentNode.SelectNodes("//tr"))
+            {
+                string name = vol.ChildNodes[1].InnerText.Trim().Replace("&nbsp;", string.Empty);
+                names.Add(name);
+                Console.WriteLine(name);
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
