f9dafb12ba4791281e0f3dc7119289a28b746f24
TymoshchukMN <TymoshchukMN@gmail.com>
2023-09-18 16:57:51 +0300

    added class starter

--- HTMLprocess/Program.cs
+++ HTMLprocess/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Xml.Linq;
 using HtmlAgilityPack;
 
 namespace HTMLprocess
@@ -12,21 +10,7 @@ namespace HTMLprocess
     {
         public static void Main(string[] args)
         {
-            var html = File.ReadAllText("E:\\data.txt", Encoding.UTF8);
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(html);
-
-            List <string> names = new List<string>();
-
-            foreach (HtmlNode vol in doc.DocumentNode.SelectNodes("//tr"))
-            {
-                string name = vol.ChildNodes[1].InnerText.Trim().Replace("&nbsp;", string.Empty);
-                names.Add(name);
-                Console.WriteLine(name);
-            }
-
-            Console.ReadKey();
+            Starter.Run(args);
         }
-
     }
 }
---
+++ HTMLprocess/Starter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace HTMLprocess
+{
+    internal class Starter
+    {
+        public static void Run(string[] htmlDoc)
+        {
+            var html = File.ReadAllText("E:\\data.txt", Encoding.UTF8);
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            List<string> names = new List<string>();
+
+            foreach (HtmlNode vol in doc.DocumentNode.SelectNodes("//tr"))
+            {
+                string name
+                    = vol.ChildNodes[1].InnerText.Trim().Replace(
+                        "&nbsp;",
+                        string.Empty);
+
+                names.Add(name);
+
+                Console.WriteLine(name);
+            }
+
+            File.WriteAllText(
+                "E:\\test.txt",
+                string.Join(
+                    names.ToString(),
+                    (char)10));
+
+            Console.ReadKey();
+        }
+    }
+}
