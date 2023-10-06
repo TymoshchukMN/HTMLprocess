307edfed89cf52be4da1bc1b08eedc9568959922
TymoshchukMN <TymoshchukMN@gmail.com>
2023-10-06 16:28:36 +0300

    adde extention AccountManagement

--- HTMLprocess/AD/GroupProcessing.cs
+++ HTMLprocess/AD/GroupProcessing.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
 
 namespace HTMLprocess.AD
 {
     internal class GroupProcessing
     {
+        public void RemoveUserFromGroup(string userId, string groupName)
+        {
+            try
+            {
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "COMPANY"))
+                {
+                    GroupPrincipal group = GroupPrincipal.FindByIdentity(pc, groupName);
+                    group.Members.Remove(pc, IdentityType.UserPrincipalName, userId);
+                    group.Save();
+                }
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                //doSomething with E.Message.ToString(); 
+
+            }
+        }
     }
 }
--- HTMLprocess/Starter.cs
+++ HTMLprocess/Starter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using TitleProcessing;
+using HTMLprocess.AD;
 
 namespace HTMLprocess
 {
@@ -24,8 +24,6 @@ namespace HTMLprocess
                     members.Add(new GroupMember(user, group));
                 }
             }
-
-           
         }
     }
 }
